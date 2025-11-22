89694c59d7d3fa87edb155074df33e924865f4fb
HRONZHAO <717853628@qq.com>
2025-11-22 02:11:26 +0000

    1

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask,render_template,request
+import joblib
 
-app = Flask(__name__)
+app=Flask(__name__)
 
 @app.route("/",methods=["GET","POST"])
 def index():
@@ -8,9 +9,21 @@ def index():
 
 @app.route("/main",methods=["GET","POST"])
 def main():
-    q = request.form.get("q")
-    print(q)
+    q=request.form.get("q")
     return(render_template("main.html"))
 
-if __name__ == "__main__":
+@app.route("/dbs",methods=["GET","POST"])
+def dbs():
+    q=request.form.get("q")
+    return(render_template("dbs.html"))
+
+@app.route("/dbs_prediction",methods=["GET","POST"])
+def dbs_prediction():
+    q=float(request.form.get("q"))
+    model=joblib.load("dbs.pkl")
+    r=model.predict([[q]])
+
+    return(render_template("dbs_prediction.html",r=r[0][0]))
+
+if __name__=="__main__":
     app.run()
